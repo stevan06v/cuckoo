a084420e845b7cb405c940f2e918b342f441926a
Michi4 <michael.ruep@gmail.com>
2025-11-23 16:03:53 +0100

    fix: deployment working now

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
 if [ ! -f "vendor/autoload.php" ]; then
+    echo "Installing Composer dependencies..."
     composer install --no-progress --no-interaction
 fi
 
 if [ ! -f ".env" ]; then
-    echo "Creating env file for env $APP_ENV"
+    echo "Creating .env file for env $APP_ENV"
     cp .env.example .env
 else
     echo "env file exists."
 fi
 
-role=${CONTAINER_ROLE:-app}
+echo "Waiting for database connection..."
+while ! nc -z db 3306; do   
+    sleep 1 
+done 
+echo "Database is ready. Starting application setup..."
 
-if [ "$role" = "app" ]; then
-    php artisan migrate --seed
-    php artisan key:generate
-    php artisan cache:clear
-    php artisan config:clear
-    php artisan route:clear
-    php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
-    exec docker-php-entrypoint "$@"
-elif [ "$role" = "queue" ]; then
-    echo "Running the queue ... "
-    php /var/www/artisan queue:work --verbose --tries=3 --timeout=180
-elif [ "$role" = "websocket" ]; then
-    echo "Running the websocket server ... "
-    php artisan websockets:serve
-fi
+chmod -R 777 storage bootstrap/cache
+
+echo "Running key generation, migrations, and seeding..."
+
+php artisan key:generate
+php artisan config:clear
+php artisan route:clear
+php artisan cache:clear
+
+php artisan migrate:fresh --force --seed
+
+exec docker-php-entrypoint "$@"
